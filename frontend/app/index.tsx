d26f61f6c455df96ece7de8ce55a14bd729f9f6d
adb8 <abuahmed0821@gmail.com>
2025-02-08 13:48:52 -0500

    third commit

---
+++ frontend/app/index.tsx
@@ -0,0 +1,33 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import "react-native-reanimated";
+import "../globals.css";
+import { Link } from "expo-router";
+import { Text, ScrollView } from "react-native";
+
+import { useColorScheme } from "@/hooks/useColorScheme";
+
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return <Text className="text-white">Top-level page</Text>;
+}
